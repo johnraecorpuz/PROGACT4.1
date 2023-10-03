b3b81344542e04e0c6ed5ec6cc6ce4ea6b81df0e
johnraecorpuz <qjrlscorpuz@tip.edu.ph>
2023-10-03 08:12:03 +0000

    Corpuz

---
+++ test.py
@@ -0,0 +1,22 @@
+import math
+
+def test_sqrt():
+    num=12
+    assert math.sqrt(num) == 3
+
+def test_square():
+    num = 5
+    assert num * num == 49
+
+def test_check():
+    x = 100
+    assert x >= 100
+
+def test_fun():
+    x = 6
+    assert x == 6
+
+def test_multiply():
+    x = 4
+    y = 5
+    assert x * y == 20
